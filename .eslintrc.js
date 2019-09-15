c50c6f61dfe88d60d010c57741c52e19c6888ef4
Viktor Ivarsson <vicke.i93@gmail.com>
2019-09-15 13:36:09 +0200

    chore: update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'eslint:recommended',
-    'prettier/@typescript-eslint',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: ['react-app'],
+  plugins: ['react-hooks'],
   rules: {
-    '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-member-accessibility': 'off',
-    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'prettier/prettier': 'off',
